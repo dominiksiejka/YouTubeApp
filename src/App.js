199ed8f1142855e1fe795ea396982e6bdf3f6fdf
sieja182 <dev@dominiksiejka.pl>
2021-06-14 17:14:05 +0200

    create App.js file and add create its layout there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import VideoList from "./VideoList";
+import SearchBar from "./SearchBar";
+import youtube from "./youtube";
+import DisplayedVideo from "./DisplayedVideo";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.divRef = React.createRef();
+  }
+  state = {
+    fetchedData: [],
+    selectedVideo: "",
+  };
+  handleFormSubmit = async (inputVal) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: inputVal,
+      },
+    });
+    if (response) {
+      this.setState({
+        fetchedData: response.data.items,
+        selectedVideo: "",
+      });
+    } else return;
+  };
+  componentDidMount = () => {
+    this.handleFormSubmit("sia");
+  };
+  handleSelectedVideo = (currentVideo) => {
+    const clickedItem = [...this.state.fetchedData][currentVideo];
+    this.setState({
+      selectedVideo: clickedItem,
+    });
+    setTimeout(() => {
+      const item = this.divRef.current;
+      item.scrollIntoView({ behavior: "smooth" });
+    }, 0);
+  };
+  render() {
+    const ifSelectedVideo = this.state.selectedVideo && (
+      <DisplayedVideo video={this.state.selectedVideo} refe={this.divRef} />
+    );
+    return (
+      <div className='container'>
+        <SearchBar handleFormSubmit={this.handleFormSubmit} />
+        {ifSelectedVideo}
+        <VideoList
+          data={this.state.fetchedData}
+          handleSelectedVideo={this.handleSelectedVideo}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
